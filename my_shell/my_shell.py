bb8c1d0db6c29a584566c89d0937c76c720ffca3
student <ngarci2032@gmail.com>
2021-02-20 18:45:15 -0700

    completed pwd, cd, and in/out redirection

--- my_shell/my_shell.py
+++ my_shell/my_shell.py
@@ -3,8 +3,38 @@ import sys
 import re
 from my_read_line import my_read_line
 
+def out_redir(args):
+    os.close(1)
+    os.open(args[args.index('>') + 1], os.O_WRONLY | os.O_CREAT)
+    os.set_inheritable(1,True)
+    args.remove(args[args.index('>') + 1])
+    args.remove('>')
+    for dir in re.split(":",os.environ['PATH']):  #try each directory
+        program = "%s/%s" % (dir, args[0])
+        try:
+            os.execve(program, args, os.environ) #try to execute program
+        except FileNotFoundError:
+            pass
+    os.write(2, ("Command not found\n").encode())
+    sys.exit(1)
+
+def in_redir(args):
+    os.close(0)
+    os.open(args[args.index('<') + 1], os.O_RDONLY)
+    os.set_inheritable(1,True)
+    args.remove(args[args.index('<') + 1])
+    args.remove('<')
+    for dir in re.split(":",os.environ['PATH']):  #try each directory
+        program = "%s/%s" % (dir, args[0])
+        try:
+            os.execve(program, args, os.environ) #try to execute program
+        except FileNotFoundError:
+            pass
+    os.write(2, ("Command not found\n").encode())
+    sys.exit(1)
+
 while(1):
-    os.write(1, "$ ".encode())
+    os.write(1, "\n$ ".encode())
     
     input = my_read_line()
 
@@ -13,21 +43,42 @@ while(1):
         sys.exit(1)
 
     args = input.split() #tokenized user input
-    rc = os.fork()       #forks a child process
+    
+    curr_dir = os.getcwd()
 
-    if rc < 0:
-        os.write(2,("Fork failed %d\n" % rc).encode())
-        sys.exit(1)
-
-    elif rc == 0: 
-        for dir in re.split(":",os.environ['PATH']):  #try each directory
-            program = "%s/%s" % (dir, args[0])
-            try:
-                os.execve(program, args, os.environ) #try to execute program
-            except FileNotFoundError:
-                pass
-        os.write(2, ("Command not found\n").encode())
-        sys.exit(1)
+    if args[0] == "pwd":
+        os.write(2, (os.getcwd()).encode())
+    
+    elif args[0] == "cd":
+        try:
+            os.chdir(args[1])
+            os.write(2, (os.getcwd()).encode())
+            
+        except FileNotFoundError:
+            pass
+            os.write(2, ("Error: File not found...").encode())
+        
     else:
-        child_pid = os.wait() #waiting for child to finish
+        rc = os.fork()       #forks a child process
+    
+        if rc < 0:
+            os.write(2,("Fork failed %d\n" % rc).encode())
+            sys.exit(1)
+
+        elif rc == 0:
+            if '>' in args:
+                out_redir(args)
+            elif '<' in args:
+                in_redir(args)
+            else:
+                for dir in re.split(":",os.environ['PATH']):  #try each directory
+                    program = "%s/%s" % (dir, args[0])
+                    try:
+                        os.execve(program, args, os.environ) #try to execute program
+                    except FileNotFoundError:
+                        pass
+                os.write(2, ("Command not found\n").encode())
+                sys.exit(1)
+        else:
+            child_pid = os.wait() #waiting for child to finish
         
